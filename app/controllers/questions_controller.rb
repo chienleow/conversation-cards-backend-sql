a4b8e35d18a01efabf41ac068c470d5c6a68219a
chienleow <chienleow1@gmail.com>
2021-01-15 00:02:47 -0500

    add methods to user and question controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::QuestionsController < ApplicationController
+    def index
+        questions = Question.all
+        render json: QuestionSerializer.new(questions)
+    end
+
+    def create
+        @question = Question.new(question_params)
+        if @question.save
+            render json: @question, status: :accepted
+        else
+            render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+    
+    def question_params
+        params.require(:question).permit(:sentence, :category, :user_id)
+    end
 end
