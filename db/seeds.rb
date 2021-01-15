11b704dc877bdba865b31c9cd749c7f76aa054e5
chienleow <chienleow1@gmail.com>
2021-01-14 23:57:38 -0500

    add seed data, add user_id attribute to question table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rachel = User.create(name: "Rachel")
+monica = User.create(name: "Monica")
+phoebe = User.create(name: "Phoebe")
+
+question1 = Question.create(sentence: "Would you like to be famous? In what way?", category: "Fun", user_id: rachel.id)
+question2 = Question.create(sentence: "Would you rather have a dog with a cat’s personality or a cat with a dog’s personality?", category: "Would you rather", user_id: monica.id)
+question3 = Question.create(sentence: "What will finally break the internet?", category: "Random", user_id: phoebe.id)
+question4 = Question.create(sentence: "Would you rather be transported permanently 500 years into the future or 500 years into the past?", category: "Would you rather", user_id: rachel.id)
+question5 = Question.create(sentence: "What's something not many people know about you?", category: "Deep", user_id: monica.id)
+question6 = Question.create(sentence: "What was your worst fashion disaster?", category: "Fun", user_id: phoebe.id)
+
